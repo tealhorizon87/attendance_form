4a27927cc2130e94a36c8408e92359521d7eee38
tealhoion87 <nealtoms@gmail.com>
2022-01-14 17:21:27 +0000

    Add another option to diet and a third guest
      - add an 'other' option in the diet selectors and allow the user to
        enter their own dietary info
      - add a third guest section to the form
      - add the js required to show the input for the 'other' option,
        and to add that and the third guest to the email
      - adjust the text in the lead paragraph, and add a date and menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,67 @@
-function sendMail(contactForm) {
+const otherDiet = document.getElementById("otherDiet");
+const otherDiet1 = document.getElementById("otherDiet1");
+const otherDiet2 = document.getElementById("otherDiet2");
+const otherDiet3 = document.getElementById("otherDiet3");
 
+function dietSelector(that) {
+  if (that.name == "diet") {
+    if (that.value == "other") {
+      otherDiet.style.display = "block";
+    }
+  } else if (that.name == "guest_1_diet") {
+    if (that.value == "other") {
+      otherDiet1.style.display = "block";
+    }
+  } else if (that.name == "guest_2_diet") {
+    if (that.value == "other") {
+      otherDiet2.style.display = "block";
+    }
+  } else if (that.name == "guest_3_diet") {
+    if (that.value == "other") {
+      otherDiet3.style.display = "block";
+    }
+  }
+}
+
+
+function sendMail(contactForm) {
   var formData = {
     "name": contactForm.name.value,
     "dining": contactForm.dining.value,
-    "diet": contactForm.diet.value,
+  };
+
+  if (contactForm.diet.value == "other") {
+    formData["diet"] = otherDiet.value;
   };
 
   if (contactForm.guest_1_name.value) {
-    formData["guest1"] = `${contactForm.guest_1_name.value} is ${contactForm.guest_1_dining.value}. Their dietary requirements are - ${contactForm.guest_1_diet.value}.`;
+    if (contactForm.guest_1_diet.value == "other") {
+      formData["guest1"] = `${contactForm.guest_1_name.value} is ${contactForm.guest_1_dining.value}.
+        Their dietary requirements are - Other: ${otherDiet1.value}.`;
+    } else {
+      formData["guest1"] = `${contactForm.guest_1_name.value} is ${contactForm.guest_1_dining.value}.
+        Their dietary requirements are - ${contactForm.guest_1_diet.value}.`;
+    }
   };
 
   if (contactForm.guest_2_name.value) {
-    formData["guest2"] = `${contactForm.guest_2_name.value} is ${contactForm.guest_2_dining.value}. Their dietary requirements are - ${contactForm.guest_2_diet.value}.`;
+    if (contactForm.guest_2_diet.value == "other") {
+      formData["guest2"] = `${contactForm.guest_2_name.value} is ${contactForm.guest_2_dining.value}.
+        Their dietary requirements are - Other: ${otherDiet2.value}.`;
+    } else {
+      formData["guest2"] = `${contactForm.guest_2_name.value} is ${contactForm.guest_2_dining.value}.
+        Their dietary requirements are - ${contactForm.guest_2_diet.value}.`;
+    }
+  };
+
+  if (contactForm.guest_3_name.value) {
+    if (contactForm.guest_3_diet.value == "other") {
+      formData["guest3"] = `${contactForm.guest_3_name.value} is ${contactForm.guest_3_dining.value}.
+        Their dietary requirements are - Other: ${otherDiet3.value}.`;
+    } else {
+      formData["guest3"] = `${contactForm.guest_3_name.value} is ${contactForm.guest_3_dining.value}.
+        Their dietary requirements are - ${contactForm.guest_3_diet.value}.`;
+    }
   };
 
   emailjs.send("service_2p76xzg", "attendace_form", formData)
