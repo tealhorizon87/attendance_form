867a5e50083e94687de92693aba832c164587704
Neal Toms <nealtoms@gmail.com>
2023-08-18 15:44:11 +0100

    Update for September meeting
    Add menu for september meeting
    
    Add second email to reply to sender to hopefully stop double subs

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,37 +42,37 @@ function sendMail(contactForm) {
 
   if (contactForm.guest_1_name.value) {
     if (contactForm.guest_1_diet.value == "other") {
-      formData["guest1"] = `${contactForm.guest_1_name.value} is ${contactForm.guest_1_dining.value}.
+      formData["guest1"] = `${contactForm.guest_1_name.value}: ${contactForm.guest_1_dining.value}.
         Their dietary requirements are - Other: ${otherDiet1.value}.`;
     } else if (contactForm.guest_1_dining.value == "dining") {
-      formData["guest1"] = `${contactForm.guest_1_name.value} is ${contactForm.guest_1_dining.value}.
+      formData["guest1"] = `${contactForm.guest_1_name.value}: ${contactForm.guest_1_dining.value}.
         Their dietary requirements are - ${contactForm.guest_1_diet.value}.`;
     } else {
-      formData["guest1"] = `${contactForm.guest_1_name.value} is ${contactForm.guest_1_dining.value}.`;
+      formData["guest1"] = `${contactForm.guest_1_name.value}: ${contactForm.guest_1_dining.value}.`;
     }
   };
 
   if (contactForm.guest_2_name.value) {
     if (contactForm.guest_2_diet.value == "other") {
-      formData["guest2"] = `${contactForm.guest_2_name.value} is ${contactForm.guest_2_dining.value}.
+      formData["guest2"] = `${contactForm.guest_2_name.value}: ${contactForm.guest_2_dining.value}.
         Their dietary requirements are - Other: ${otherDiet2.value}.`;
       } else if (contactForm.guest_2_dining.value == "dining") {
-        formData["guest2"] = `${contactForm.guest_2_name.value} is ${contactForm.guest_2_dining.value}.
+        formData["guest2"] = `${contactForm.guest_2_name.value}: ${contactForm.guest_2_dining.value}.
           Their dietary requirements are - ${contactForm.guest_2_diet.value}.`;
       } else {
-        formData["guest2"] = `${contactForm.guest_2_name.value} is ${contactForm.guest_2_dining.value}.`;
+        formData["guest2"] = `${contactForm.guest_2_name.value}: ${contactForm.guest_2_dining.value}.`;
       }
   };
 
   if (contactForm.guest_3_name.value) {
     if (contactForm.guest_3_diet.value == "other") {
-      formData["guest3"] = `${contactForm.guest_3_name.value} is ${contactForm.guest_3_dining.value}.
+      formData["guest3"] = `${contactForm.guest_3_name.value}: ${contactForm.guest_3_dining.value}.
         Their dietary requirements are - Other: ${otherDiet3.value}.`;
       } else if (contactForm.guest_3_dining.value == "dining") {
-        formData["guest3"] = `${contactForm.guest_3_name.value} is ${contactForm.guest_3_dining.value}.
+        formData["guest3"] = `${contactForm.guest_3_name.value}: ${contactForm.guest_3_dining.value}.
           Their dietary requirements are - ${contactForm.guest_3_diet.value}.`;
       } else {
-        formData["guest3"] = `${contactForm.guest_3_name.value} is ${contactForm.guest_3_dining.value}.`;
+        formData["guest3"] = `${contactForm.guest_3_name.value}: ${contactForm.guest_3_dining.value}.`;
       }
   };
 
@@ -86,5 +86,13 @@ function sendMail(contactForm) {
       alert("I'm sorry, something went wrong! Please try again");
       return error;
     });
+
+  emailjs.send("service_2p76xzg", "template_qadn8oc", formData)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+
   return false;
 }
