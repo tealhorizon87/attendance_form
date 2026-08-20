94efa62804ef43d282733474fd992897a8e517b3
Neal Toms <nealtoms@gmail.com>
2026-08-20 16:28:15 +0100

    Add pudding options to all guests

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,10 +59,10 @@ function attendingForm(contactForm) {
   if (contactForm.guest_1_name.value) {
     if (contactForm.guest_1_diet.value == "other") {
       formData["guest1"] = `${contactForm.guest_1_name.value}: ${contactForm.guest_1_dining.value}.
-        Their dietary requirements are - Other: ${otherDiet1.value}.`;
+        Their dietary requirements are - Other: ${otherDiet1.value}. Their pudding option is ${contactForm.guest1PuddingOption.value}.`;
     } else if (contactForm.guest_1_dining.value == "dining") {
       formData["guest1"] = `${contactForm.guest_1_name.value}: ${contactForm.guest_1_dining.value}.
-        Their dietary requirements are - ${contactForm.guest_1_diet.value}.`;
+        Their dietary requirements are - ${contactForm.guest_1_diet.value}. Their pudding option is ${contactForm.guest1PuddingOption.value}.`;
     } else {
       formData["guest1"] = `${contactForm.guest_1_name.value}: ${contactForm.guest_1_dining.value}.`;
     }
@@ -71,10 +71,10 @@ function attendingForm(contactForm) {
   if (contactForm.guest_2_name.value) {
     if (contactForm.guest_2_diet.value == "other") {
       formData["guest2"] = `${contactForm.guest_2_name.value}: ${contactForm.guest_2_dining.value}.
-        Their dietary requirements are - Other: ${otherDiet2.value}.`;
+        Their dietary requirements are - Other: ${otherDiet2.value}. Their pudding option is ${contactForm.guest2PuddingOption.value}.`;
       } else if (contactForm.guest_2_dining.value == "dining") {
         formData["guest2"] = `${contactForm.guest_2_name.value}: ${contactForm.guest_2_dining.value}.
-          Their dietary requirements are - ${contactForm.guest_2_diet.value}.`;
+          Their dietary requirements are - ${contactForm.guest_2_diet.value}. Their pudding option is ${contactForm.guest2PuddingOption.value}.`;
       } else {
         formData["guest2"] = `${contactForm.guest_2_name.value}: ${contactForm.guest_2_dining.value}.`;
       }
@@ -83,10 +83,10 @@ function attendingForm(contactForm) {
   if (contactForm.guest_3_name.value) {
     if (contactForm.guest_3_diet.value == "other") {
       formData["guest3"] = `${contactForm.guest_3_name.value}: ${contactForm.guest_3_dining.value}.
-        Their dietary requirements are - Other: ${otherDiet3.value}.`;
+        Their dietary requirements are - Other: ${otherDiet3.value}. Their pudding option is ${contactForm.guest3PuddingOption.value}.`;
       } else if (contactForm.guest_3_dining.value == "dining") {
         formData["guest3"] = `${contactForm.guest_3_name.value}: ${contactForm.guest_3_dining.value}.
-          Their dietary requirements are - ${contactForm.guest_3_diet.value}.`;
+          Their dietary requirements are - ${contactForm.guest_3_diet.value}. Their pudding option is ${contactForm.guest3PuddingOption.value}.`;
       } else {
         formData["guest3"] = `${contactForm.guest_3_name.value}: ${contactForm.guest_3_dining.value}.`;
       }
